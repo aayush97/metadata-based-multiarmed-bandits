6e36db2224767a0893551d602ce57181a63c5a4c
abporter521 <70152974+abporter521@users.noreply.github.com>
2023-04-08 13:46:32 -0500

    Added Regret and Reward Plots

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -2,6 +2,7 @@
 import random as rand
 import heapq
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Create Random bandit machine with unknown mean and variance
 class Bandit:
@@ -24,7 +25,7 @@ class Bandit:
             return 0
     
     def show_mean(self):
-        return ("The true mean for Bandit ", self.id_no, " is ", self.true_mean)
+        return (self.id_no, self.true_mean)
     
     def guess(self, guess):
         '''
@@ -48,16 +49,15 @@ class TSBernoulli_Agent:
         self.decay = decay_rate
         self.epsilon_end = epsilon_end
         self.action_count = 0
+        self.cum_reward = []
 
     def choose_bandit(self):
         '''
         Sample each known bandit by its variance distribution and select
         max estimate with probability epsilon
-        '''
-        
+        '''     
         highest_estimate_id = 0
         highest_estimate = None
-
 
         for bandit in self.bandit_interactions.keys():
             #Get the variance of the bandit
@@ -94,11 +94,15 @@ class TSBernoulli_Agent:
         a, b, c, rc = self.bandit_interactions[id]
 
         if reward == 1:
-            self.bandit_interactions[id] = (a + 1, b, c + 1, rc+reward) 
-        
+            self.bandit_interactions[id] = (a + 1, b, c + 1, rc+reward)         
         else: # reward is 0
             self.bandit_interactions[id] = (a, b + 1, c + 1, rc) 
         
+        # Record cumulative reward
+        if len(self.cum_reward) > 0:
+          self.cum_reward.append(self.cum_reward[-1] +reward)
+        else:
+          self.cum_reward.append(reward)
         return
 
     def add_bandit(self, id, bandit_alpha = 1, bandit_beta = 1 ):
@@ -118,10 +122,23 @@ class TSBernoulli_Agent:
         estimates = list()
 
         for key in self.bandit_interactions:
-
+          if self.bandit_interactions[key][2] != 0:
             estimates.append((key,self.bandit_interactions[key][0]/self.bandit_interactions[key][2]))
-        
+          else:
+            estimates.append((key, 0))
         return estimates
+    
+    def get_cum_reward(self):
+      '''
+      Return the cumulative reward for the agent
+      '''
+      return self.cum_reward[-1]
+
+    def get_indexed_cum_reward(self):
+      '''
+      Return the list of reward throught agent experience
+      '''
+      return self.cum_reward
 
 if __name__ == '__main__':
     threshold = 0.05
@@ -129,16 +146,22 @@ if __name__ == '__main__':
     decay = 0.995
     epsilon_end = 0.0
     bandits_count = 10
-    episodes = 5000
+    episodes = 100
 
     bandits = {}
     agent = TSBernoulli_Agent(epsilon, decay, epsilon_end)
 
+    best_bandit = None
+    cum_best_reward = []
+
+    # Initialize the bandits and find which has the best mean
     for i in range(bandits_count):
         x = Bandit(i, threshold)
         print(x.show_mean())
         bandits[i] = x
         agent.add_bandit(i)
+        if best_bandit == None or x.show_mean()[1] > best_bandit.show_mean()[1]:
+            best_bandit = x
 
     for e in range(episodes):
         choice = agent.choose_bandit()
@@ -146,11 +169,19 @@ if __name__ == '__main__':
         reward = bandits[choice].sample()
         agent.update_estimates(reward, choice)
 
+        # For regret calculation
+        best_reward = best_bandit.sample()
+        if len(cum_best_reward) > 0:
+          cum_best_reward.append(cum_best_reward[-1] + best_reward)
+        else:
+          cum_best_reward.append(best_reward)
+
 
     # Check our work
     count_correct=0
     est = agent.show_estimates()
     print(est)
+    cum_reward = agent.get_indexed_cum_reward()
     
     # Does not get approximation for every bandit
     # When it gets a correct approximation, it is because agent
@@ -167,3 +198,29 @@ if __name__ == '__main__':
         print("Found the top achievers")
     else:
         print("Not quite.")
+
+    # Plot the regret for basic Thompson Sampling
+    regret = []
+    for k in range(episodes):
+      if len(regret) > 0:
+        regret.append(regret[-1] + (cum_best_reward[k] - cum_reward[k]))
+      else:
+        regret.append((cum_best_reward[k] - cum_reward[k]))
+    
+    # Plot 
+    fig = plt.figure()
+    
+    ax = fig.add_subplot(2,1,1)    
+    plt.title("Perfomance Bernoulli Bandits")
+    plt.plot(range(len(cum_best_reward)), cum_best_reward, label="Optimal Cum Reward") 
+    plt.plot(range(len(cum_reward)), cum_reward, label="Agent Cum Reward")
+    plt.ylabel('Cumulative Score')
+    ax.legend()
+    
+    bx = fig.add_subplot(2,1,2)
+    plt.plot(range(len(regret)), regret, label="Regret")
+    plt.ylabel('Regret(Sum [best - agent])')
+    plt.xlabel('Pull #')
+     
+    plt.legend(loc='best')
+    plt.show()

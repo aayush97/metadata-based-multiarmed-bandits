90bf4124c747790ab1f10161f7e58547714cb09d
abporter521 <70152974+abporter521@users.noreply.github.com>
2023-04-11 14:04:19 -0500

    Changed the Bandit to Multi-Arm

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -5,42 +5,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Create Random bandit machine with unknown mean and variance
-class Bandit:
-    def __init__(self, id_no, estimation_threshold):
+class BernoulliBandit:
+    def __init__(self, arms_no, id_no, estimation_threshold):
         '''
-        true_mean: The true reward probability of receiving reward from this bandit
+        arms: A list of each arm and it's mean at index: arm number
+        id_no: ID number for the bandit
+        Arms_no: number of arms this bandit has
         '''
-        self.true_mean = rand.random()
+        self.arms = []
+        # Give each arm of the bandit a mean
+        for arm in range(arms_no):
+            self.arms.append(rand.random())
+
         self.id_no = id_no
         self.threshold = estimation_threshold
+        self.arms_no = arms_no
+
+        self.best_arm = np.argmax(self.arms)
     
-    def sample(self):
+
+    def sample(self, arm):
         '''
         Returns: Bernoulli reward with probability of the mean of the Bandit
         '''
-        if rand.random() < self.true_mean:
+        # Compares with mean of arm
+        if rand.random() < self.arms[arm]:
             return 1
         
         else:
             return 0
     
-    def show_mean(self):
-        return (self.id_no, self.true_mean)
-    
-    def guess(self, guess):
+    def guess(self, guess, arm):
         '''
         Guess: A float of the estimated mean for the bandit
         
         Returns:
             True if value is within threshold, False otherwise
         '''
-        isTrue = abs(self.true_mean - guess)
+        isTrue = abs(self.arms[arm] - guess)
 
         if isTrue <= self.threshold:
             return True
         
         return False
-
+    
+    def get_best_arm(self):
+        return self.best_arm
 # This Agent plays the game and makes estimates on the 
 class TSBernoulli_Agent:
     def __init__(self, epsilon, decay_rate, epsilon_end):
@@ -51,52 +61,55 @@ class TSBernoulli_Agent:
         self.action_count = 0
         self.cum_reward = []
 
-    def choose_bandit(self):
+    def choose_arm(self, sample_list):
         '''
-        Sample each known bandit by its variance distribution and select
-        max estimate with probability epsilon
+        Returns List of best arm number for each known bandit
+        Sample_list: A list of the ID numbers of Bandits to choose
+        '''
+        best_arm_idx_list = []
+
+        for bandit in sample_list:
+            best_idx = 0 
+            best_reward = 0
+            idx = 0
+
+            # For every bandit in our sample list, sample the best arm
+            for alpha, beta in self.bandit_interactions[bandit][0]:
+ 
+                x = np.random.beta(alpha, beta)
+                if x > best_reward:
+                    best_reward = x
+                    best_idx = idx
+                idx += 1      
+
+            best_arm_idx_list.append(best_idx)
+        
+        return best_arm_idx_list
+    
+    def choose_bandit(self, sample_no):
+        '''
+        Returns a number of bandits to test
+        Sample_no: The number of bandits to test
         '''     
-        highest_estimate_id = 0
-        highest_estimate = None
+        sample = np.random.choice(list(self.bandit_interactions.keys()), size=sample_no)
 
-        for bandit in self.bandit_interactions.keys():
-            #Get the variance of the bandit
-            bandit_alpha = self.bandit_interactions[bandit][0]
-            bandit_beta = self.bandit_interactions[bandit][1]
-            
-            #Get a random number for the estimate variance
-            x = np.random.beta(bandit_alpha, bandit_beta)
-            
-            #If it is the highest, save it
-            if highest_estimate is None or x > highest_estimate:
-                highest_estimate_id = bandit
-                highest_estimate = x                                          
+        return sample
 
-        self.action_count += 1
-        if self.action_count % 10 == 0:
-            self.epsilon = max(self.epsilon_end, self.epsilon * self.decay)
-
-        # Explore   
-        if rand.random() < self.epsilon:
-            b = rand.randint(0, len(self.bandit_interactions)-1)           
-            return b
-
-        # Take the greedy choice
-        else:
-            return highest_estimate_id
-
-    def update_estimates(self, reward, id):
+    def update_estimates(self, reward, id, arm_no):
         '''
         Reward: The reward received from a bandit
         ID: The id number of the bandit
+        Arm_no: The arm of bandit ID that was pulled
         '''
-        #Update reward count and visit count
-        a, b, c, rc = self.bandit_interactions[id]
-
+        #Update List, reward count and visit count
+        distribution, c, rc = self.bandit_interactions[id]
+        #Update Alpha Beta count for list at arm arm_no
         if reward == 1:
-            self.bandit_interactions[id] = (a + 1, b, c + 1, rc+reward)         
-        else: # reward is 0
-            self.bandit_interactions[id] = (a, b + 1, c + 1, rc) 
+          distribution[arm_no][0] += 1
+        else:
+          distribution[arm_no][1] += 1
+        
+        self.bandit_interactions[id] = (distribution, c + 1, rc+reward)         
         
         # Record cumulative reward
         if len(self.cum_reward) > 0:
@@ -105,13 +118,17 @@ class TSBernoulli_Agent:
           self.cum_reward.append(reward)
         return
 
-    def add_bandit(self, id, bandit_alpha = 1, bandit_beta = 1 ):
+    def add_bandit(self, id, num_of_arms, arm_alpha = 1, arm_beta = 1 ):
         '''
         ID: ID number of the bandit to be added to self history
         Bandit_alpah/beta: The estimated reward from the bandit, initialized to 1 for uniform beta distribution
         '''
-        # Bandit item has tuple (alpha, beta, visit_count, cumulative_reward)
-        self.bandit_interactions[id] = (bandit_alpha, bandit_beta, 0, 0)
+        # Bandit item has tuple (Beta distribution for each arm in bandit, visit_count, cumulative_reward)
+        arm_distribution = []
+        for i in range(num_of_arms):
+            arm_distribution.append([arm_alpha, arm_beta])
+
+        self.bandit_interactions[id] = (arm_distribution, 0, 0)
         
         return False
     
@@ -134,74 +151,66 @@ class TSBernoulli_Agent:
       '''
       return self.cum_reward[-1]
 
-    def get_indexed_cum_reward(self):
-      '''
-      Return the list of reward throught agent experience
-      '''
-      return self.cum_reward
 
 if __name__ == '__main__':
     threshold = 0.05
+    num_of_arms = 20
     epsilon = 0
     decay = 0.995
     epsilon_end = 0.0
     bandits_count = 10
     episodes = 500
+    sample_no = 5
 
     bandits = {}
     agent = TSBernoulli_Agent(epsilon, decay, epsilon_end)
 
     best_bandit = None
     cum_best_reward = []
+    best_reward = []
+    reward = []
     regret = []
     bayesian_regret = 0
 
     # Initialize the bandits and find which has the best mean
     for i in range(bandits_count):
-        x = Bandit(i, threshold)
-        print(x.show_mean())
+        x = BernoulliBandit(num_of_arms, i, threshold)
         bandits[i] = x
-        agent.add_bandit(i)
-        if best_bandit == None or x.show_mean()[1] > best_bandit.show_mean()[1]:
-            best_bandit = x
+        agent.add_bandit(i, num_of_arms)
 
     for e in range(episodes):
-        choice = agent.choose_bandit()
+        choice = agent.choose_bandit(sample_no)
         #print("choice is ", choice)
-        reward = bandits[choice].sample()
-        agent.update_estimates(reward, choice)
+        best_arm_list = agent.choose_arm(choice)
+        cum_regret = 0
+        cum_rew = 0
+        rew = 0
 
-        # For regret calculation
-        best_reward = best_bandit.sample()
-        if len(cum_best_reward) > 0:
-          cum_best_reward.append(cum_best_reward[-1] + best_reward)
-        else:
-          cum_best_reward.append(best_reward)
-        
-        bayesian_regret += best_bandit.show_mean()[1] - bandits[choice].show_mean()[1]
-        regret.append(bayesian_regret)
+        for idx, bandit in enumerate(choice):
+            breward = bandits[bandit].sample(best_arm_list[idx])
+            agent.update_estimates(breward, bandit, idx)
+            rew += breward
+
+            # Calculate the Regret
+            # Sample the best arm - reward and current arm 
+            best_arm_reward = bandits[bandit].sample(bandits[bandit].get_best_arm())
+            cum_regret += (best_arm_reward - breward)
+            cum_rew += best_arm_reward
+
+        regret.append(cum_regret)
+        best_reward.append(cum_rew)
+        reward.append(rew)
+
+    cum_sum = np.cumsum(regret)
+    cum_best_reward = np.cumsum(best_reward)
 
     # Check our work
     count_correct=0
-    est = agent.show_estimates()
-    print(est)
-    cum_reward = agent.get_indexed_cum_reward()
+    cum_reward = np.cumsum(reward)
 
     # Does not get approximation for every bandit
     # When it gets a correct approximation, it is because agent
     # noticed it gave a higher reward distribution and played it again
-
-    for b, m in est:
-        y = bandits[b].guess(m)
-        print(y)
-        if y:
-            count_correct +=1
-            print("Bandit ", b, " has reward distribution ", m)
-    
-    if count_correct >= 2:
-        print("Found the top achievers")
-    else:
-        print("Not quite.")
 
     
     # Plot 
@@ -215,11 +224,9 @@ if __name__ == '__main__':
     ax.legend()
     
     bx = fig.add_subplot(2,1,2)
-    plt.plot(range(len(regret)), regret, label="Regret")
+    plt.plot(range(len(cum_sum)), cum_sum, label="Regret")
     plt.ylabel('Regret')
     plt.xlabel('Pull #')
      
     plt.legend(loc='best')
     plt.show()
-
-
